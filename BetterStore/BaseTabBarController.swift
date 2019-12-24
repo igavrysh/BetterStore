4995a6714af6949f3d18743ba12db89c3a6f6f1a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-24 09:00:07 -0800

    Add Today(Nav)Controller

--- BetterStore/BaseTabBarController.swift
+++ BetterStore/BaseTabBarController.swift
@@ -9,9 +9,21 @@
 import UIKit
 
 class BaseTabBarController: UITabBarController {
-
+    
+    // 2 - refactor out repeated logic inside of viewDidLoad
+    // 3 - maybe introduc our AppsSearchController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let todayController = UIViewController()
+        todayController.view.backgroundColor = .white
+        todayController.navigationItem.title = "Today"
+        
+        let todayNavController = UINavigationController(rootViewController: todayController)
+        todayNavController.tabBarItem.title = "Today"
+        todayNavController.navigationBar.prefersLargeTitles = true
+        todayNavController.tabBarItem.image = UIImage(named: "today")
 
         //view.backgroundColor = .white
         let redViewController = UIViewController()
@@ -36,8 +48,10 @@ class BaseTabBarController: UITabBarController {
         //tabBar.barTintColor = .green
         
         viewControllers = [
+            todayNavController,
             redNavController,
             blueNavController
         ]
     }
+
 }
