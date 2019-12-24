3f307fc7b91171d0a55ba68e5b447e3626b53b72
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-24 07:56:41 -0800

    Initial Commit

---
+++ BetterStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterStore
+//
+//  Created by new on 12/24/19.
+//  Copyright © 2019 Ievgen Gavrysh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
