8f2a003eda7c6c4d2ca9843c8cc8e5cabe5f5e79
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-24 09:42:30 -0800

    Add formatting to SearchResultCell

--- BetterStore/Views/SearchResultCell.swift
+++ BetterStore/Views/SearchResultCell.swift
@@ -10,10 +10,70 @@ import UIKit
 
 class SearchResultCell: UICollectionViewCell {
     
+    let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
+        iv.layer.cornerRadius = 12
+        return iv
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "APP NAME"
+        return label
+    }()
+    
+    let categoryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Photos & Videos"
+        return label
+    }()
+    
+    let ratingsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "9.26M"
+        return label
+    }()
+    
+    let getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        button.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .green
+        backgroundColor = .yellow
+        
+        let labelesStackView = UIStackView(arrangedSubviews: [
+            nameLabel,
+            categoryLabel,
+            ratingsLabel
+        ])
+        labelesStackView.axis = .vertical
+        
+        let stackView = UIStackView(arrangedSubviews: [
+            imageView,
+            labelesStackView,
+            getButton
+        ])
+        
+        stackView.spacing = 12
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        // To use autolayout set this to false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
     }
     
     required init?(coder: NSCoder) {
