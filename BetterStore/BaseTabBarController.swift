8689f179b471ade57a9c016f6aa3fdb40d80e9b0
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-24 08:55:50 -0800

    Changed colors for the tabs

--- BetterStore/BaseTabBarController.swift
+++ BetterStore/BaseTabBarController.swift
@@ -13,24 +13,27 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .yellow
+        //view.backgroundColor = .white
         let redViewController = UIViewController()
-        redViewController.view.backgroundColor = .red
+        redViewController.view.backgroundColor = .white
         redViewController.navigationItem.title = "Apps"
         
         let redNavController = UINavigationController(rootViewController: redViewController)
-        redNavController.tabBarItem.title = "RED NAV"
+        redNavController.tabBarItem.title = "Apps"
         redNavController.navigationBar.prefersLargeTitles = true
         redNavController.tabBarItem.image = UIImage(named: "apps")
         
         let blueViewController = UIViewController()
-        blueViewController.view.backgroundColor = .blue
+        blueViewController.view.backgroundColor = .white
         blueViewController.navigationItem.title = "Search"
         
         let blueNavController = UINavigationController(rootViewController: blueViewController)
-        blueNavController.tabBarItem.title = "BLUE NAV"
+        blueNavController.tabBarItem.title = "Search"
         blueNavController.navigationBar.prefersLargeTitles = true
         blueNavController.tabBarItem.image = UIImage(named: "search")
+        
+        //tabBar.tintColor = .yellow
+        //tabBar.barTintColor = .green
         
         viewControllers = [
             redNavController,
