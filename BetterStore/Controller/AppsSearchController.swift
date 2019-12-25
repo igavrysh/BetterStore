260bab45067fe7f17b0ac8b1b5dbfdf601db3e03
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-24 19:40:22 -0800

    Add SDWebImage and cocoa pods to the project

--- BetterStore/Controller/AppsSearchController.swift
+++ BetterStore/Controller/AppsSearchController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class AppsSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
@@ -46,6 +47,23 @@ class AppsSearchController: UICollectionViewController, UICollectionViewDelegate
         cell.nameLabel.text = appResult.trackName
         cell.categoryLabel.text = appResult.primaryGenreName
         cell.ratingsLabel.text = "Rating: \(appResult.averageUserRating ?? 0)"
+        
+        let url = URL(string: appResult.artworkUrl100)
+        cell.appIconImageView.sd_setImage(with: url)
+        
+        if (appResult.screenshotUrls.count > 0) {
+            cell.screenshot1ImageView.sd_setImage(with: URL(string: appResult.screenshotUrls[0]))
+        }
+        
+        if (appResult.screenshotUrls.count > 1) {
+            cell.screenshot2ImageView.sd_setImage(with: URL(string: appResult.screenshotUrls[1]))
+        }
+        
+        if (appResult.screenshotUrls.count > 2) {
+            cell.screenshot3ImageView.sd_setImage(with: URL(string: appResult.screenshotUrls[2]))
+
+        }
+        
         return cell
     }
 
