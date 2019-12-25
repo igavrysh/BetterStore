4fdee5a2cd873f411fcb9b0cb8d0f1d71bc05f20
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-25 10:30:22 -0800

    Source RSS feed Top Grossing Apps to UI

--- BetterStore/Controller/Apps/AppsPageController.swift
+++ BetterStore/Controller/Apps/AppsPageController.swift
@@ -13,6 +13,8 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
     let cellId = "id"
     let headerId = "headerId"
     
+    var editorsChoiceGames: AppGroup?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .white
@@ -32,8 +34,11 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
                 print("Failed to fetch games: ", err)
                 return
             }
-            
-            print(appGroup?.feed.results)
+              
+            self.editorsChoiceGames = appGroup
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         }
     }
     
@@ -47,12 +52,15 @@ class AppsPageController: BaseListController, UICollectionViewDelegateFlowLayout
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppsGroupCell
         
+        cell.titleLabel.text = editorsChoiceGames?.feed.title
+        cell.horizontalController.appGroup = editorsChoiceGames
+        cell.horizontalController.collectionView.reloadData()
         return cell
     }
 
