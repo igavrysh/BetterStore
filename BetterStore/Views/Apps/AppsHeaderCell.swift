757d13133fafeef3830433b43d8e42057ce17dc7
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-25 09:32:14 -0800

    Add header horizontal collection view to Apps tab

---
+++ BetterStore/Views/Apps/AppsHeaderCell.swift
@@ -0,0 +1,37 @@
+//
+//  AppsHeaderCell.swift
+//  BetterStore
+//
+//  Created by new on 12/25/19.
+//  Copyright © 2019 Ievgen Gavrysh. All rights reserved.
+//
+
+import UIKit
+
+class AppsHeaderCell: UICollectionViewCell {
+    
+    let companyLabel = UILabel(text: "Facebook", font: .boldSystemFont(ofSize: 12))
+    let titleLabel = UILabel(text: "Keeping up with friends is faster than ever", font: .systemFont(ofSize: 24))
+
+    let imageView = UIImageView(cornerRadius: 8)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+                
+        imageView.backgroundColor = .red
+        titleLabel.numberOfLines = 2
+        companyLabel.textColor = .blue
+        
+        let stackView = VerticalStackView(arrangedSubviews: [
+            companyLabel,
+            titleLabel,
+            imageView
+        ], spacing: 12)
+        addSubview(stackView)
+        stackView.fillSuperview(padding: .init(top: 16, left: 0, bottom: 0, right: 0))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ BetterStore/Views/Apps/AppsPageHeader.swift
@@ -0,0 +1,26 @@
+//
+//  AppsPageHeader.swift
+//  BetterStore
+//
+//  Created by new on 12/25/19.
+//  Copyright © 2019 Ievgen Gavrysh. All rights reserved.
+//
+
+import UIKit
+
+class AppsPageHeader: UICollectionReusableView {
+    
+    let appHeaderHorizontalController = AppsHeaderHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+                
+        appHeaderHorizontalController.view.backgroundColor = .purple
+        addSubview(appHeaderHorizontalController.view)
+        appHeaderHorizontalController.view.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
