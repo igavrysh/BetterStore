5fad332406d642ab770463dca3ba6dd8e22d6a3c
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2019-12-29 15:51:42 -0800

    Move close button on todys extended card to the parent view controller

--- BetterStore/Controller/Today/AppFullscreenController.swift
+++ BetterStore/Controller/Today/AppFullscreenController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AppFullscreenController: UITableViewController {
+class AppFullscreenController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var dismissHandler: (() -> ())?
     var todayItem: TodayItem?
@@ -16,8 +16,17 @@ class AppFullscreenController: UITableViewController {
     fileprivate let headerCellId = "headerCellId"
     fileprivate let descriptionCellId = "descriptionCellId"
     
+    let tableView = UITableView(frame: .zero, style: .plain)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.clipsToBounds = true
+        
+        view.addSubview(tableView)
+        tableView.fillSuperview()
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
         tableView.register(AppFullscreenHeaderCell.self, forCellReuseIdentifier: headerCellId)
@@ -25,23 +34,41 @@ class AppFullscreenController: UITableViewController {
         tableView.contentInsetAdjustmentBehavior = .never
         let height = UIApplication.shared.statusBarFrame.height
         tableView.contentInset = .init(top: 0, left: 0, bottom: height, right: 0)
+        setupCloseButton()
     }
+    
+    let closeButton: UIButton = {
+       let button = UIButton(type: .system)
+       button.setImage(UIImage(named: "close_button"), for: .normal)
+       return button
+    }()
      
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    fileprivate func setupCloseButton() {
+        view.addSubview(closeButton)
+        closeButton.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: nil,
+            bottom: nil,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 0, left: 0, bottom: 0, right: 0),
+            size: .init(width: 80, height: 40))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
-    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         if scrollView.contentOffset.y < 0 {
             scrollView.isScrollEnabled = false
             scrollView.isScrollEnabled = true
         }
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.item == 0 {
             let headerCell = tableView.dequeueReusableCell(withIdentifier: headerCellId, for: indexPath) as! AppFullscreenHeaderCell
-            headerCell.closeButton.addTarget(self, action: #selector(handleDismiss(_:)), for: .touchUpInside)
+            closeButton.addTarget(self, action: #selector(handleDismiss(_:)), for: .touchUpInside)
             headerCell.todayCell.todayItem = todayItem
             headerCell.todayCell.layer.cornerRadius = 0
             headerCell.clipsToBounds = true
@@ -52,11 +79,11 @@ class AppFullscreenController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if indexPath.row == 0 {
             return TodayController.cellSize
         }
-        return super.tableView(tableView, heightForRowAt: indexPath)
+        return UITableView.automaticDimension //tableView(tableView, heightForRowAt: indexPath)
     }
     
     @objc func handleDismiss(_ button: UIButton) {
--- BetterStore/Views/Today/AppFullscreenHeaderCell.swift
+++ BetterStore/Views/Today/AppFullscreenHeaderCell.swift
@@ -12,17 +12,13 @@ class AppFullscreenHeaderCell: UITableViewCell {
     
     let todayCell = TodayCell()
     
-    let closeButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "close_button"), for: .normal)
-        return button
-    }()
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         addSubview(todayCell)
         todayCell.fillSuperview()
+        
+        /*
         addSubview(closeButton)
         closeButton.anchor(
           top: topAnchor,
@@ -30,6 +26,7 @@ class AppFullscreenHeaderCell: UITableViewCell {
           bottom: nil,
           trailing: trailingAnchor,
           padding: .init(top: 44, left: 0, bottom: 0, right: 12), size: .init(width: 80, height: 38))
+        */
     }
     
     required init?(coder: NSCoder) {
